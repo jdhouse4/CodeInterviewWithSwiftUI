2c865ee8f5b3baac3de68d6b10a5cfb802edaeb0
Jim Hillhouse <portablefrontier@me.com>
2021-04-06 22:41:15 -0500

    Initial Commit

---
+++ CodeInterviewWithSwiftUI/CodeInterviewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeInterviewWithSwiftUIApp.swift
+//  CodeInterviewWithSwiftUI
+//
+//  Created by James Hillhouse IV on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeInterviewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
