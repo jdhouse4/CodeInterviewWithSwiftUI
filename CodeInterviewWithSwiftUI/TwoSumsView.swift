77124c41633f0a3fee6bb47432847d2ee39fa9c5
Jim Hillhouse <portablefrontier@me.com>
2021-05-13 16:10:43 -0500

    More cleanup

--- CodeInterviewWithSwiftUI/TwoSumsView.swift
+++ CodeInterviewWithSwiftUI/TwoSumsView.swift
@@ -52,7 +52,7 @@ struct TwoSumsView: View {
                         Enter an array of positive, real or integer numbers
                         (seperated by ", ") :
                         """)
-                        .frame(width: 400, alignment: .leading)
+                        .frame(width: 350, alignment: .leading)
                         .padding(.top, 100)
                         .padding(.leading, 5)
 
@@ -71,7 +71,7 @@ struct TwoSumsView: View {
                             self.array = readArrayEntry(from: self.numberString)
                             self.arrayEntered = true
                         }
-                        .frame(width: 250)
+                        .frame(width: 200)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .multilineTextAlignment(.trailing)
                         .minimumScaleFactor(0.5)
@@ -89,10 +89,10 @@ struct TwoSumsView: View {
                         .frame(width: 100, height: 30, alignment: .leading)
                         .padding([.top, .leading], 5)
                         .padding(.trailing, 20)
-                        .padding(.top, 10)
+                        //.padding(.top, 10)
 
                     TextField("Your array will appear here.", text: $arrayString)
-                        .frame(width: 250)
+                        .frame(width: 200)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .multilineTextAlignment(.trailing)
                         .minimumScaleFactor(0.5)
